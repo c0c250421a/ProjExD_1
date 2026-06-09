8e4d451f0b05fc095f844ce878aba9d5a5ee12ae
tajimaf <c0c250421a@edu.teu.ac.jp>
2026-06-09 16:09:53 +0900

    練習10:こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bgf_img = pg.transform.flip(bg_img,True,False)#背景反転
     koukaton = pg.image.load("fig/3.png")#こうかとんSurfaceの作成
     koukaton = pg.transform.flip(koukaton,True,False)#左右反転
+    koukaton_rect = koukaton.get_rect()#rectを取得
+    koukaton_rect.center = 300,200#初期座標を設定
     tmr = 0
     x = 0
     while True:
@@ -23,7 +25,16 @@ def main():
         screen.blit(bg_img, [-x, 0])#動かす
         screen.blit(bgf_img,[-(x)+1600,0])#もう一枚
         screen.blit(bg_img, [-x+3200, 0])#動かす2
-        screen.blit(koukaton,[300,200])#こうかとんを描画
+        screen.blit(koukaton,koukaton_rect)#こうかとんを描画
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            koukaton_rect.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            koukaton_rect.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            koukaton_rect.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            koukaton_rect.move_ip((1, 0))
         pg.display.update()
         tmr += 1    
         clock.tick(200)#練習6
