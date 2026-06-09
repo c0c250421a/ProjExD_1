9a8b3c8ba04702ec89f3c737e3baf5e9071702a1
tajimaf <c0c250421a@edu.teu.ac.jp>
2026-06-09 15:33:20 +0900

    練習3と4:こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    koukaton = pg.image.load("fig/3.png")#こうかとんSurfaceの作成
+    koukaton = pg.transform.flip(koukaton,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(koukaton,[300,200])#こうかとんを描画
         pg.display.update()
         tmr += 1        
         clock.tick(10)
